7d972fd81314b173d0a29f7b136bc960868b67a8
James Stewart <jys@ketlai.co.uk>
2011-08-16 11:36:12 +0100

    Extend test coverage to include new GET method

--- test/panopticon_test.rb
+++ test/panopticon_test.rb
@@ -12,16 +12,29 @@ class PanopticonTest < Test::Unit::TestCase
   end
 
   def test_it_returns_an_http_201_for_success
-    params = {:name => 'james', :owning_app => 'guides'}
+    params = {:name => 'james', :owning_app => 'guides', :kind => 'guide'}
     post '/slugs', :slug => params
     assert_equal 201, last_response.status
   end
   
   def test_it_returns_an_http_406_for_duplicate_slug
-    params = {:name => 'james', :owning_app => 'guides'}
+    params = {:name => 'james', :owning_app => 'guides', :kind => 'blah'}
     post '/slugs', :slug => params
     post '/slugs', :slug => params
     assert_equal 406, last_response.status
+  end
+
+  def test_get_returns_details_as_json
+    params = {:name => 'another-james', :owning_app => 'guides', :kind => 'blah'}
+    post '/slugs', :slug => params
+    get '/slugs/another-james'
+    assert_equal 200, last_response.status
+    assert_equal 'blah', JSON.parse(last_response.body)['kind']
+  end
+    
+  def test_get_returns_an_http_404_for_unknown_slug
+    get '/slugs/something-new'
+    assert_equal 404, last_response.status
   end
   
 end
