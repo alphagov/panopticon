97379b825d708a351c989133536fc754ba7521dc
James Stewart <jys@ketlai.co.uk>
2011-08-16 11:31:28 +0100

    Panopticon now knows what kind of thing a slug represents. We also have a new method /slugs/{slug} to check if a slug is taken and if so, get its details

--- README.md
+++ README.md
@@ -8,12 +8,20 @@ so that we have unique URLs.
 
 ### Slug Service
 
-eg.
-
-POST /slugs
-slug[name]=my-slug&slug[owning_app]=my_app
+`POST /slugs
+slug[name]=my-slug&slug[owning_app]=my_app`
 
 Expected responses are:
 
-201 Created : The slug was available and ownership of it has been recorded
-406 Not Acceptable : The slug was already taken
+* 201 Created : The slug was available and ownership of it has been recorded
+* 406 Not Acceptable : The slug was already taken
+
+`GET /slugs/{slug}`
+
+Expected responses are:
+
+* 200 OK : Slug exists. Returns JSON encoded details, eg. {"slug":"my-slug","owning_app":"guides","kind":"guide","active":true}
+* 404 Not Found: Slug does not exist
+
+Note that you should check the 'active' value of a returned slug to see if it's currently in use. Inactive slugs will not be routed.
+
--- panopticon.rb
+++ panopticon.rb
@@ -8,22 +8,48 @@ DataMapper::setup(:default, File.read(File.expand_path("../config/database.txt",
 class Identifier
   include DataMapper::Resource
   
-  property :id, Serial
-  property :active, Boolean, :default => false, :required => true
-  property :slug, String, :unique => true, :required => true, :length => 4..32
-  property :owning_app, String, :required => true
+  property :id,         Serial
+  property :active,     Boolean, :default => false, :required => true
+  property :slug,       String,  :unique => true,   :required => true, :length => 4..32
+  property :owning_app, String,  :required => true
+  property :kind,       String,  :required => true
   property :created_at, DateTime
 end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
+class Hash
+  def slice(*keys)
+    allowed = Set.new(respond_to?(:convert_key) ? keys.map { |key| convert_key(key) } : keys)
+    hash = {}
+    allowed.each { |k| hash[k] = self[k] if has_key?(k) }
+    hash
+  end
+end
+
 post '/slugs' do
-  new_resource = Identifier.new(:slug => params[:slug][:name], :owning_app => params[:slug][:owning_app], :active => true)
+  new_resource = Identifier.new(
+    :slug => params[:slug][:name], 
+    :owning_app => params[:slug][:owning_app], 
+    :active => true,
+    :kind => params[:slug][:kind]
+  )
 
   if new_resource.save
     status 201
   else
     status 406
+  end
+end
+
+get '/slugs/:id' do
+  resource = Identifier.first(slug: params[:id])
+  if resource
+    content_type :json
+    
+    resource.attributes.slice(:slug, :owning_app, :kind).to_json
+  else
+    status 404
   end
 end
