e8c7db26a67612807d45b63c2d9584031ea7f728
James Stewart <jys@ketlai.co.uk>
2011-09-01 10:06:06 +0100

    Fix tests so that we don't get sequencing issues

--- test/panopticon_test.rb
+++ test/panopticon_test.rb
@@ -11,20 +11,20 @@ class PanopticonTest < Test::Unit::TestCase
   end
 
   def test_it_returns_an_http_201_for_success
-    params = {:name => 'james', :owning_app => 'guides', :kind => 'guide'}
+    params = {:name => 'james1', :owning_app => 'guides', :kind => 'guide'}
     post '/slugs', :slug => params
     assert_equal 201, last_response.status
   end
   
   def test_it_returns_an_http_406_for_duplicate_slug
-    params = {:name => 'james', :owning_app => 'guides', :kind => 'blah'}
+    params = {:name => 'james2', :owning_app => 'guides', :kind => 'blah'}
     post '/slugs', :slug => params
     post '/slugs', :slug => params
     assert_equal 406, last_response.status
   end
 
   def test_get_returns_details_as_json
-    params = {:name => 'another-james', :owning_app => 'guides', :kind => 'blah'}
+    params = {:name => 'james3', :owning_app => 'guides', :kind => 'blah'}
     post '/slugs', :slug => params
     get '/slugs/another-james'
     assert_equal 200, last_response.status
