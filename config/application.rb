d3df55ce8de61a495f3ce5b76e8b7f46893d3717
Craig R Webster <craig@barkingiguana.com>
2011-10-19 15:10:31 +0100

    Try inserting middleware in application.rb

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Panopticon
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.insert 0, Slimmer::App, :template_host => "#{config.root}/public/templates"
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-use "Slimmer::App", :template_host => "#{File.dirname(__FILE__)}/../public/templates"
-
 # Initialize the rails application
 Panopticon::Application.initialize!
