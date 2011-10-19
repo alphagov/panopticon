a547f35c1502f642dbafa5b6519c9b47e6a3c8f3
Craig R Webster <craig@barkingiguana.com>
2011-10-19 16:01:01 +0100

    Get round locking problem with Slimmer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,6 @@ Panopticon::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.middleware.insert 0, Slimmer::App, :template_host => "/data/vhost/static.production.alphagov.co.uk/current/public/templates"
+  config.middleware.delete Slimmer::App
+  config.middleware.use Slimmer::App, :template_host => "/data/vhost/static.production.alphagov.co.uk/current/public/templates"
 end
