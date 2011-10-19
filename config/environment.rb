6625c5f5d20f0a809b1da76a4e99698a098a885a
Craig R Webster <craig@barkingiguana.com>
2011-10-19 15:07:25 +0100

    Try inserting middleware in application.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.middleware.insert 0, "Slimmer::App", :template_host => "#{config.root}/public/templates"
+use "Slimmer::App", :template_host => "#{config.root}/public/templates"
 
 # Initialize the rails application
 Panopticon::Application.initialize!
