cbf3c8d53cd425266b4b3723bb9867535e453d3a
Tom Stuart <tom@experthuman.com>
2011-11-01 13:44:11 +0000

    Add FakeTransport

---
+++ features/support/fake_transport.rb
@@ -0,0 +1,26 @@
+class FakeTransport
+  include Singleton
+
+  attr_reader :notifications
+
+  def initialize
+    flush
+  end
+
+  def flush
+    self.notifications = []
+  end
+
+  def publish(destination, message, headers = {})
+    notifications << { destination: destination, message: Hash.from_xml(message).with_indifferent_access, headers: headers }
+  end
+
+  private
+    attr_writer :notifications
+end
+
+Messenger.transport = FakeTransport.instance
+
+Before do
+  FakeTransport.instance.flush
+end
