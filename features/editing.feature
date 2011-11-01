41b65926015ac52e1670b01b84cb10add12291c9
Tom Stuart <tom@experthuman.com>
2011-11-01 12:01:40 +0000

    Add "When I add '...' as a related item" step

--- features/editing.feature
+++ features/editing.feature
@@ -7,3 +7,4 @@ Feature: Editing artefacts
       And an artefact exists with a name of "Leaving prison"
 
     When I am editing "Probation"
+      And I add "Leaving prison" as a related item
--- features/step_definitions/artefact_steps.rb
+++ features/step_definitions/artefact_steps.rb
@@ -1,3 +1,11 @@
 When /^I am editing "(.*)"$/ do |name|
   visit edit_artefact_path(Artefact.find_by_name(name))
 end
+
+When /^I add "(.*)" as a related item$/ do |name|
+  within_fieldset 'Related items' do
+    within :xpath, './/select[not(option[@selected])]' do
+      select name
+    end
+  end
+end
