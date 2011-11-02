eedec3d76cf1bf2b7f0c35b46d3ea5082dcbee32
Tom Stuart <tom@experthuman.com>
2011-11-02 15:03:30 +0000

    Add Artefact#to_param to match Artefact.from_param

--- app/models/artefact.rb
+++ app/models/artefact.rb
@@ -109,6 +109,10 @@ class Artefact < ActiveRecord::Base
     Messenger.instance.updated self
   end
 
+  def to_param
+    slug
+  end
+
   def self.from_param(slug_or_id)
     # FIXME: A hack until the Publisher has panopticon ids for every article
     find_by_slug(slug_or_id) || find(slug_or_id)
