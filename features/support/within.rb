8963724b351385573458c48ad1cec9bef85df55f
Tom Stuart <tom@experthuman.com>
2011-11-03 14:36:04 +0000

    Simplify within_select_with_selection with XPath::HTML.option

--- features/support/within.rb
+++ features/support/within.rb
@@ -3,5 +3,5 @@ def within_select_with_no_selection(&block)
 end
 
 def within_select_with_selection(value, &block)
-  within :xpath, XPath.generate { |x| x.descendant(:select)[x.descendant(:option)[x.attr(:selected)] == value] }, &block
+  within :xpath, XPath.descendant(:select)[XPath::HTML.option(value)], &block
 end
