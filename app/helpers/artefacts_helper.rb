70cfd6658d4f113156480eb79e5e744b2cd544fc
Tom Stuart <tom@experthuman.com>
2011-11-03 11:55:59 +0000

    Generalise related_item_with_sort_key for other associations

--- app/helpers/artefacts_helper.rb
+++ app/helpers/artefacts_helper.rb
@@ -1,12 +1,11 @@
 module ArtefactsHelper
   def related_items_for(artefact)
     (0...Artefact::MAXIMUM_RELATED_ITEMS).
-      map { |sort_key| related_item_with_sort_key(artefact.related_items, sort_key) }
+      map { |sort_key| associated_record_with_sort_key(artefact.related_items, sort_key) }
   end
 
   private
-    def related_item_with_sort_key(related_items, sort_key)
-      related_items.detect { |related_item| related_item.sort_key == sort_key } ||
-        RelatedItem.new(:sort_key => sort_key)
+    def associated_record_with_sort_key(association, sort_key)
+      association.detect { |record| record.sort_key == sort_key } || association.new(:sort_key => sort_key)
     end
 end
