2ee57ad1efe6354a66d950ee51061027396398bf
Tom Stuart <tom@experthuman.com>
2011-11-10 11:26:01 +0000

    Connect Stomp client when running rake tasks

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,6 +1,6 @@
 require 'messenger'
 
-unless File.basename($0) == 'rake' || Rails.env.test?
+unless Rails.env.test?
   host = Rails.env.production? ? 'support.cluster' : 'localhost'
   uri = URI::Generic.build scheme: 'stomp', host: host, port: 61613
   Messenger.transport = Stomp::Client.new uri.to_s
