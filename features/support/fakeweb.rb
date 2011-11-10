9ca66b41b551fed303155b7e98f360f0cdaaeec8
Tom Stuart <tom@experthuman.com>
2011-11-10 15:15:47 +0000

    Fake public_bodies.json response from Imminence
    This matches the change in 2d0f6fd972504ae36a2958a938cfc9aeeca90929.

--- features/support/fakeweb.rb
+++ features/support/fakeweb.rb
@@ -1,2 +1,3 @@
 FakeWeb.allow_net_connect = false
 FakeWeb.register_uri :get, %r{^#{Regexp.escape Plek.current.find('publisher')}/}, :status => [200, 'OK']
+FakeWeb.register_uri :get, "#{Plek.current.find('data')}/data_sets/public_bodies.json", :body => [].to_json
