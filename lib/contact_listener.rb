2c0fe3bf8be42ecc73e9e89939cc4cbac085e85c
Tom Stuart <tom@experthuman.com>
2011-11-15 09:01:05 +0000

    Add ContactListener

---
+++ lib/contact_listener.rb
@@ -0,0 +1,45 @@
+class ContactListener
+  def process(message)
+    logger.info "Received message #{message}"
+
+    begin
+      logger.info "Processing contact #{message['id']}"
+      yield Contact.find_or_initialize_by_contactotron_id(message['id'])
+    rescue => e
+      logger.error "Unable to process message #{message}"
+      logger.error [e.message, e.backtrace].flatten.join("\n")
+    end
+
+    logger.info "Finished processing message #{message}"
+  end
+
+  def listen
+    Signal.trap 'TERM' do
+      client.close
+      exit
+    end
+
+    marples = Messenger.instance.client
+
+    marples.when 'contactotron', '*', 'created' do |message|
+      process message do |contact|
+        logger.info "Creating contact #{message['id']}"
+        contact.update_attributes! :name => message['name']
+      end
+    end
+
+    marples.when 'contactotron', '*', 'updated' do |message|
+      process message do |contact|
+        logger.info "Updating contact #{message['id']}"
+        contact.update_attributes! :name => message['name']
+      end
+    end
+
+    logger.info 'Listening for created and updated contacts in Contact-o-tron'
+    marples.join
+  end
+
+  def logger
+    @logger ||= Logger.new(STDOUT).tap { |logger| logger.level = Logger::DEBUG }
+  end
+end
