b3e0c8aefa2cb3b6c85d67c384f31dcb746ff268
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-11-16 17:19:38 +0000

    reverting back to a static list & decouppling from imminence this is only tempoary as this field is going to become managed by need-o-tron soon

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,10 +1,15 @@
 class Department
-  # FIXME: This couples Panopticon operation to Imminence operation which I
-  # don't like. Panopticon should probably denormalise this data every time
-  # the data set is updated and just use that denormalised data here.
+  # FIXME: This is only temporary - this field will shortly be managed via
+  # need-o-tron and synced via marples/message queue
   def self.all
-    data_set_url = Plek.current.find("data") + '/data_sets/public_bodies.json'
-    data_set = JSON.parse open(data_set_url).read
-    data_set.map { |department| department['id'] }.sort
+    [
+      "BIS - Adult Education",
+      "BIS - Employment",
+      "DCLG",
+      "DWP - Benefits",
+      "DWP - Pensions, disabled and carers",
+      "MoJ",
+      "GDS"
+    ]
   end
 end
