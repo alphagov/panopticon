e955036a561ee56711344bc890a9b0e874c36d29
Tom Stuart <tom@experthuman.com>
2011-11-17 14:07:08 +0000

    Always configure marples_client_name and marples_logger
    These weren't being configured in the test environment, which caused failures
    when the tests were run in a directory that wasn't called "panopticon" (e.g.
    in Jenkins).

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -2,8 +2,6 @@ require 'active_record_ext'
 require 'messenger'
 unless Rails.env.test?
   stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
-  ActiveRecord::Base.marples_client_name = 'panopticon'
-  ActiveRecord::Base.marples_logger = Rails.logger
 
   if defined?(PhusionPassenger)
     PhusionPassenger.on_event(:starting_worker_process) do |forked|
--- lib/active_record_ext.rb
+++ lib/active_record_ext.rb
@@ -1,5 +1,8 @@
 require 'marples/model_action_broadcast'
+
 ActiveRecord::Base.class_eval do
   include Marples::ModelActionBroadcast
-  
 end
+
+ActiveRecord::Base.marples_client_name = 'panopticon'
+ActiveRecord::Base.marples_logger = Rails.logger
