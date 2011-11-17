a7e3af654dfdbb2eb13bccfa7d9ebf866b1adca7
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-11-17 11:27:23 +0000

    extending active record to make panopticon noisy

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,7 +1,19 @@
+require 'active_record_ext'
 require 'messenger'
-
 unless Rails.env.test?
-  host = Rails.env.production? ? 'support.cluster' : 'localhost'
-  uri = URI::Generic.build scheme: 'stomp', host: host, port: 61613
-  Messenger.transport = Stomp::Client.new uri.to_s
+  stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
+  ActiveRecord::Base.marples_client_name = 'panopticon'
+  ActiveRecord::Base.marples_logger = Rails.logger
+
+  if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      if forked
+        Messenger.transport = Stomp::Client.new stomp_url    
+        ActiveRecord::Base.marples_transport = Stomp::Client.new stomp_url
+      end
+    end
+  else
+    Messenger.transport = Stomp::Client.new stomp_url
+    ActiveRecord::Base.marples_transport = Stomp::Client.new stomp_url
+  end
 end
