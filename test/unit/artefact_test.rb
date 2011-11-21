9c1e7b5d2d79f33abf50e410da9ff463dd275e9d
James Stewart <jys@ketlai.co.uk>
2011-11-21 20:23:17 +0000

    Some basic tests to make sure artefacts have well-formed slugs

---
+++ test/unit/artefact_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ArtefactTest < ActiveSupport::TestCase
+  test "it allows nice clean slugs" do
+    a = Artefact.new(:slug => "its-a-nice-day")
+    a.valid?
+    assert a.errors[:slug].empty?
+  end
+  
+  test "it doesn't allow apostrophes in slugs" do
+    a = Artefact.new(:slug => "it's-a-nice-day")
+    assert ! a.valid?
+    assert a.errors[:slug].any?
+  end
+  
+  test "it doesn't allow spaces in slugs" do
+    a = Artefact.new(:slug => "it is-a-nice-day")
+    assert ! a.valid?
+    assert a.errors[:slug].any?
+  end
+end
