ea02d34b6a4327d7397f7ad4b2e98a07f7e0a4e4
Tom Stuart <tom@experthuman.com>
2011-11-21 12:58:36 +0000

    Implement Contact#update_from_contactotron

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,26 @@
+require 'open-uri'
+
 class Contact < ActiveRecord::Base
   validates :name, :presence => true
 
   scope :in_alphabetical_order, order('name ASC')
+
+  def update_from_contactotron
+    update_attributes! data_from_contactotron.slice(:name, :postal_address, :email_address, :website_url, :opening_hours)
+  end
+
+  private
+    def contactotron_uri
+      URI.parse(Plek.current.find('contactotron')).tap do |uri|
+        uri.path = "/contacts/#{contactotron_id}.json"
+      end
+    end
+
+    def json_from_contactotron
+      open(contactotron_uri.to_s).read
+    end
+
+    def data_from_contactotron
+      ActiveSupport::JSON.decode(json_from_contactotron).with_indifferent_access
+    end
 end
