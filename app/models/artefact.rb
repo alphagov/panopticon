957c18cb90c1453f8a143e5ebbb0cbf7d7489bf0
Craig R Webster <craig@barkingiguana.com>
2011-12-05 14:54:44 +0000

    Include Related items by default in the XML

--- app/models/artefact.rb
+++ app/models/artefact.rb
@@ -23,9 +23,9 @@ class Artefact < ActiveRecord::Base
     "place"
   ].freeze
 
-  has_many :related_items, :foreign_key => :source_artefact_id, :order => 'sort_key ASC', :dependent => :destroy 
-  has_many :reverse_related_items, :foreign_key => :artefact_id, :class_name => 'RelatedItem', :order => 'sort_key ASC', :dependent => :destroy 
-  has_many :related_artefacts, :through => :related_items, :source => :artefact  
+  has_many :related_items, :foreign_key => :source_artefact_id, :order => 'sort_key ASC', :dependent => :destroy
+  has_many :reverse_related_items, :foreign_key => :artefact_id, :class_name => 'RelatedItem', :order => 'sort_key ASC', :dependent => :destroy
+  has_many :related_artefacts, :through => :related_items, :source => :artefact
   belongs_to :contact
   has_and_belongs_to_many :audiences
 
@@ -70,7 +70,7 @@ class Artefact < ActiveRecord::Base
   end
 
   def to_xml options = {}, &blk
-    options[:include] = [ :audiences ] unless options.key? :include
+    options[:include] = [ :audiences, :related_items ] unless options.key? :include
     super options, &blk
   end
 end
