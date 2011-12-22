8776cdffe2e00801d6dd46d097f5119c532586c3
Gareth Rushgrove <gareth@morethanseven.net>
2011-12-22 10:49:04 +0000

    readd this helper as otherwise all the tests that rely on it fail

--- features/support/api.rb
+++ features/support/api.rb
@@ -1,3 +1,8 @@
+def artefact_data_from_api(artefact)
+  visit artefact_path(artefact, :format => :json)
+  JSON.parse(source).with_indifferent_access
+end
+
 def related_artefact_ids_from_api(artefact)
   artefact_data_from_api(artefact)[:related_items].map { |related_item| related_item[:artefact][:id] }
 end
