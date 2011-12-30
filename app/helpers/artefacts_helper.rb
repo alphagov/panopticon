d15441e8c365e521f39f6952326ab0c29210f153
James Stewart <jys@ketlai.co.uk>
2011-12-30 10:56:13 +0000

    Open need-o-tron link in new window

--- app/helpers/artefacts_helper.rb
+++ app/helpers/artefacts_helper.rb
@@ -4,6 +4,10 @@ module ArtefactsHelper
       map { |sort_key| associated_record_with_sort_key(artefact.related_items, sort_key) }
   end
 
+  def need_url(artefact)
+    Plek.current.find('needotron') + "/needs/#{artefact.need_id}"
+  end
+
   private
     def associated_record_with_sort_key(association, sort_key)
       association.detect { |record| record.sort_key == sort_key } || association.new(:sort_key => sort_key)
