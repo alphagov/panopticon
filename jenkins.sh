938e126b3000d0f64a34a8b31e3c7cd566cad3fa
Jordan Hatch <jordan@jordanh.net>
2012-01-10 11:56:02 +0000

    Drop and re-create blank database each time tests run.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:schema:load
+bundle exec rake db:drop db:create db:schema:load
 bundle exec rake
