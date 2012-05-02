9c697967929b456f57a5a668dc47d4e3a0caf4b1
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-02 15:18:18 +0100

    Pull Marples config into the to_prepare hook.
    Since this now operates on the Artefact class, rather than
    ActiveRecord::Base, it got merrily discarded when the app reloaded
    in development mode.

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,20 +1,24 @@
 require 'messenger'
 
-if Rails.env.test? or ENV['NO_MESSENGER'].present?
-  Messenger.transport = Marples::NullTransport.instance
-  Artefact.marples_transport = Marples::NullTransport.instance
-else
-  stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
-
-  if defined?(PhusionPassenger)
-    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-      if forked
-        Messenger.transport = Stomp::Client.new stomp_url
-        Artefact.marples_transport = Stomp::Client.new stomp_url
-      end
-    end
+# Marples config needs to be triggered by on_prepare so it doesn't get merrily
+# blatted whenever the Artefact model reloads in development mode
+Panopticon::Application.config.to_prepare do
+  if Rails.env.test? or ENV['NO_MESSENGER'].present?
+    Messenger.transport = Marples::NullTransport.instance
+    Artefact.marples_transport = Marples::NullTransport.instance
   else
-    Messenger.transport = Stomp::Client.new stomp_url
-    Artefact.marples_transport = Stomp::Client.new stomp_url
+    stomp_url = "failover://(stomp://support.cluster:61613,stomp://support.cluster:61613)"
+
+    if defined?(PhusionPassenger)
+      PhusionPassenger.on_event(:starting_worker_process) do |forked|
+        if forked
+          Messenger.transport = Stomp::Client.new stomp_url
+          Artefact.marples_transport = Stomp::Client.new stomp_url
+        end
+      end
+    else
+      Messenger.transport = Stomp::Client.new stomp_url
+      Artefact.marples_transport = Stomp::Client.new stomp_url
+    end
   end
 end
