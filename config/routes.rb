bd26c146f75acc764b0ab58c305ef4462a7161f1
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2012-05-03 12:33:37 +0100

    Allow unescaped slashes in tag IDs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Panopticon::Application.routes.draw do
   resources :artefacts
 
-  resources :tags, :defaults => { :format => 'json' }
+  resources :tags, :defaults => {:format => 'json'}
+  match 'tags/:id' => 'tags#show', :id =>  /[^\.]+/, :defaults => {:format => 'json'}
 
   match 'google_insight' => 'seo#show'
 
