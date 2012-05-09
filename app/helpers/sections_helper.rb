5f2107133780c3af17a46a679a9fb6fecbb51620
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-05-09 11:32:51 +0100

    Annotate ambiguous sections with their IDs.
    This problem should go away as we migrate the section data in future
    and, for instance, have a single 'Children' section.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -2,6 +2,18 @@ module SectionsHelper
 
   def all_sections
     section_tags = TagRepository.load_all(:type => 'section')
-    section_tags.map { |tag| [tag.title, tag.tag_id] }
+
+    title_counts = Hash.new(0)
+    section_tags.each { |tag| title_counts[tag.title] += 1 }
+
+    section_tags.map do |tag|
+      # Annotate tags, where necessary, with their IDs
+      if title_counts[tag.title] > 1
+        display_name = "#{tag.title} [#{tag.tag_id}]"
+      else
+        display_name = tag.title
+      end
+      [display_name, tag.tag_id]
+    end
   end
 end
