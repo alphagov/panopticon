51489c4617c27d46ddd786f3ad14294bac5893f8
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2012-05-15 14:40:48 +0100

    Remove primary section from sections selection.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,12 +1,12 @@
 module SectionsHelper
 
-  def all_sections
+  def all_sections(options = {})
     section_tags = TagRepository.load_all(:type => 'section')
 
     title_counts = Hash.new(0)
     section_tags.each { |tag| title_counts[tag.title] += 1 }
 
-    section_tags.map do |tag|
+    sections = section_tags.map do |tag|
       # Annotate tags, where necessary, with their IDs
       if title_counts[tag.title] > 1
         display_name = "#{tag.title} [#{tag.tag_id}]"
@@ -15,5 +15,9 @@ module SectionsHelper
       end
       [display_name, tag.tag_id]
     end
+
+    return sections unless options[:except]
+    sections.reject { |s| s[1] == options[:except] }
   end
+
 end
