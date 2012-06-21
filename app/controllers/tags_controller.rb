904de8f82b6ef981b72c43d63c958140da0be5ae
James Stewart <jys@ketlai.co.uk>
2012-06-21 12:23:01 +0100

    Move to Ruby 1.9 hash syntax

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,14 +4,14 @@ class TagsController < ApplicationController
   def index
     @tags = TagRepository.load_all
     # TODO: links: [next_page, prev_page]
-    respond_with(:status => 'ok', :total => @tags.count, :from => 0, :to => @tags.count - 1,
-                 :pagesize => @tags.count, :results => @tags)
+    respond_with(status: 'ok', total: @tags.count, from: 0, to: @tags.count - 1,
+      pagesize: @tags.count, results: @tags)
   end
 
   def show
     @tag = TagRepository.load(params[:id])
     if @tag
-      respond_with(:status=>'ok', :tag => @tag)
+      respond_with(status: 'ok', tag: @tag)
     else
       head :not_found
     end
