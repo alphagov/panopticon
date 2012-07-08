c7e7a402f0aecd86f70270ea0bdf94ca49bcb967
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-07-08 19:56:00 +0100

    Avoid an error at boot time in dev

--- config/initializers/current_release_sha.rb
+++ config/initializers/current_release_sha.rb
@@ -1,6 +1,6 @@
-revision = `cat #{Rails.root}/REVISION`.chomp
-if revision.blank?
-  CURRENT_RELEASE_SHA = "development"
-else
+if File.exists?("#{Rails.root}/REVISION")
+  revision = `cat #{Rails.root}/REVISION`.chomp
   CURRENT_RELEASE_SHA = revision[0..10] # Just get the short SHA
+else
+  CURRENT_RELEASE_SHA = "development"
 end
