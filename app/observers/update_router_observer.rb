6feb7de5fb2635e31726502242b96af5605d3563
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-08-15 15:31:06 +0000

    Add explaintative comments

--- app/observers/update_router_observer.rb
+++ app/observers/update_router_observer.rb
@@ -3,6 +3,8 @@ class UpdateRouterObserver < Mongoid::Observer
 
   def after_save(artefact)
     RoutableArtefact.new(artefact).submit if artefact.live?
+    # Relying on current behaviour where this does not raise errors
+    # if done more than once, or done on artefacts never put live
     RoutableArtefact.new(artefact).delete if artefact.archived?
   end
 end
