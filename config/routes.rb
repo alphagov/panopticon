912e9ebe83e0e6fb052ffa5690994f64a0ceba2a
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-10-10 00:28:15 +0100

    Remove no longer used curated lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Panopticon::Application.routes.draw do
   resources :artefacts, :constraints => { :id => /[^\.]+/ }
   resources :tags, :defaults => {:format => 'json'}
 
-  resources :curated_lists, only: :index
-
   match 'tags/:id' => 'tags#show', :id =>  /[^\.]+/, :defaults => {:format => 'json'}
 
   match 'google_insight' => 'seo#show'
