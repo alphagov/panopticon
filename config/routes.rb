d0e1204be8fbd5cd801d8f5834543b512d3757ec
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-10-10 16:36:20 +0100

    Enable editing of sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Panopticon::Application.routes.draw do
   resources :artefacts, :constraints => { :id => /[^\.]+/ }
-  resources :tags, :defaults => {:format => 'json'}
+  resources :tags
 
   match 'tags/:id' => 'tags#show', :id =>  /[^\.]+/, :defaults => {:format => 'json'}
-
+  match 'categories' => 'tags#categories'
   match 'google_insight' => 'seo#show'
   match 'curated_list' => 'curated_list#import'
   match 'curated_list/create' => 'curated_list#create'
