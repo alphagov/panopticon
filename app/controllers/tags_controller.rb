751c05af3adbfa630062ac7babfcb6f6ac9c6060
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-10-25 16:19:07 +0100

    Any request to tags redirect to categories
    Remove ability to make json request. We have the content api so there's no need for this anymore. 
    
    Removing redundant controller code, we will be writing a proper tag admin.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,19 +1,8 @@
 class TagsController < ApplicationController
-  respond_to :json, :html
+  respond_to :html
 
   def index
-    @tags = TagRepository.load_all(tag_type: params[:type])
-    respond_with(status: 'ok', total: @tags.count, from: 0, to: @tags.count - 1,
-      pagesize: @tags.count, results: @tags)
-  end
-
-  def show
-    @tag = TagRepository.load(params[:id])
-    if @tag
-      respond_with(status: 'ok', tag: @tag)
-    else
-      head :not_found
-    end
+    redirect_to categories_path
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Panopticon::Application.routes.draw do
   resources :artefacts, :constraints => { :id => /[^\.]+/ }
   resources :tags
 
-  match 'tags/:id' => 'tags#show', :id =>  /[^\.]+/, :defaults => {:format => 'json'}
   match 'categories' => 'tags#categories', :as => :categories
   match 'google_insight' => 'seo#show'
   match 'curated_list' => 'curated_list#import', :as => :curated_list
