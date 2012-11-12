fc06d61021caed5cfc5f90baf33c5894f8d3a7f7
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-11-12 12:46:10 +0000

    Change ...registry-offices to ...register-office
    This to be deployed in conjunction with a redirect in Puppet.

--- db/seeds/tags.rb
+++ db/seeds/tags.rb
@@ -6,3 +6,9 @@
 
 TagRepository.put(:tag_id => 'businesslink', :title => 'Business Link', :tag_type => 'legacy_source')
 TagRepository.put(:tag_id => 'directgov', :title => 'Directgov', :tag_type => 'legacy_source')
+
+old_registry_office_tag = Tag.where(:tag_id => 'births-deaths-marriages/registry-offices').first
+if old_registry_office_tag
+  old_registry_office_tag.tag_id = 'births-deaths-marriages/register-offices'
+  old_registry_office_tag.save!
+end
