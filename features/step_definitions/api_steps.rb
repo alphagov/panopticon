4866b8869023a7942749b9d640dfba52427e1735
Philip Potter <philipgpotter@gmail.com>
2012-11-21 12:26:54 +0000

    remove old contacts.feature

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -15,14 +15,6 @@ Then /^the API should say that more of the artefacts are related$/ do
   check_artefact_does_not_have_related_artefact_in_api @artefact, @unrelated_artefact
 end
 
-Then /^the API should say that the artefact has the contact$/ do
-  check_artefact_has_contact_in_api @artefact, @contact
-end
-
-Then /^the API should say that the artefact does not have the contact$/ do
-  check_artefact_does_not_have_contact_in_api @artefact, @contact
-end
-
 Then /^the API should say that the artefact has the section$/ do
   check_artefact_has_tag_in_api @artefact, @section.tag_id
 end
