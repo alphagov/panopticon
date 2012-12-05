35c412576cda49b892919d92279bc38ace2d589f
Max Griffiths <maxgriff@thoughtworks.com>
2012-12-05 17:19:37 +0000

    Revert "updating whenever to inc govuk_setenv"
    This reverts commit 3f3e3c54e3adb8a050259339759cff4d3ea8686d.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
-govuk_setenv panopticon env RAILS_ENV=test bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-govuk_setenv panopticon env RAILS_ENV=test bundle exec rake db:drop db:create db:schema:load
-govuk_setenv panopticon env RAILS_ENV=test bundle exec rake
+export RAILS_ENV=test
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:drop db:create db:schema:load
+bundle exec rake
