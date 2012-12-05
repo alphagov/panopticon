1109eb0dcbedfba6aa3a8dda1d28f4be968d72b4
Max Griffiths <maxgriff@thoughtworks.com>
2012-12-05 16:53:28 +0000

    updating whenever call to include govuk_setenv

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
-export RAILS_ENV=test
-bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:drop db:create db:schema:load
-bundle exec rake
+govuk_setenv panopticon env RAILS_ENV=test bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+govuk_setenv panopticon env RAILS_ENV=test bundle exec rake db:drop db:create db:schema:load
+govuk_setenv panopticon env RAILS_ENV=test bundle exec rake
