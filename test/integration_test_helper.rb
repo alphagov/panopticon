ba1aaaeb5de78901424f010781c2bc4ce4eae482
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-01-21 15:41:24 +0000

    Rationalise database_cleaner setup.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -1,17 +1,10 @@
 require_relative 'test_helper'
 require 'capybara/rails'
 
-DatabaseCleaner.strategy = :truncation
-
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
 
-  setup do
-    DatabaseCleaner.clean
-  end
-
   teardown do
-    DatabaseCleaner.clean
     Capybara.use_default_driver
   end
 
