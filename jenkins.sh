299f88deaaf499e0a79cc618ed6578df409a5e82
Mazz Mosley <mazzmosley@GDSMazz.local>
2013-01-22 15:14:54 +0000

    Remove govuk_setenv we don't need it here

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-govuk_setenv panopticon env RAILS_ENV=test bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-govuk_setenv panopticon env RAILS_ENV=test bundle exec rake db:drop db:create db:schema:load
-govuk_setenv panopticon env RAILS_ENV=test bundle exec rake
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:drop db:create db:schema:load
+bundle exec rake
