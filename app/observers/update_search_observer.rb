fa4c3da24eac31aac1bea3e6857d475cf0273fd9
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-20 17:57:47 +0000

    Small readability changes

--- app/observers/update_search_observer.rb
+++ app/observers/update_search_observer.rb
@@ -21,7 +21,7 @@ class UpdateSearchObserver < Mongoid::Observer
 
     not_a_new_record = ! old_kind.nil?
     not_a_new_record &&
-        (! RummageableArtefact::FORMATS_NOT_TO_INDEX.include?(old_kind)) &&
-        RummageableArtefact::FORMATS_NOT_TO_INDEX.include?(new_kind)
+        (RummageableArtefact::FORMATS_NOT_TO_INDEX.exclude?(old_kind)) &&
+         RummageableArtefact::FORMATS_NOT_TO_INDEX.include?(new_kind)
   end
 end
