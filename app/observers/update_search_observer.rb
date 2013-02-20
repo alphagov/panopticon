9c94432a4831319e474bd3f14ce2bedd19013bb5
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-20 15:23:02 +0000

    Delete artefacts when changed to nonindexed kinds
    We have had some artefacts created (incorrectly) as Answers when they should
    have been CompletedTransactions. These were not removed automatically. This
    change would ensure that they are in future.

--- app/observers/update_search_observer.rb
+++ app/observers/update_search_observer.rb
@@ -3,9 +3,25 @@ class UpdateSearchObserver < Mongoid::Observer
 
   def after_save(artefact)
     rummageable_artefact = RummageableArtefact.new(artefact)
+    
     rummageable_artefact.submit if rummageable_artefact.should_be_indexed?
+
+    if artefact.live? && becoming_nonindexed_kind?(artefact)
+      rummageable_artefact.delete
+    end
+
     # Relying on current behaviour where this does not raise errors
     # if done more than once, or done on artefacts never put live
     rummageable_artefact.delete if artefact.archived?
+  end
+
+  def becoming_nonindexed_kind?(artefact)
+    old_kind = artefact.kind_was
+    new_kind = artefact.kind
+
+    not_a_new_record = ! old_kind.nil?
+    not_a_new_record &&
+        (! RummageableArtefact::FORMATS_NOT_TO_INDEX.include?(old_kind)) &&
+        RummageableArtefact::FORMATS_NOT_TO_INDEX.include?(new_kind)
   end
 end
