25cb90c661d0ccf321d207b3eb1ca3530ddc73e7
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-26 15:32:58 +0000

    Use a released version of govuk_content_models

--- app/helpers/artefacts_helper.rb
+++ app/helpers/artefacts_helper.rb
@@ -14,4 +14,12 @@ module ArtefactsHelper
   def name_hint_for(artefact)
     artefact.persisted? ? "This should be edited in #{artefact.owning_app}" : "A name/title for the item"
   end
+
+  def non_whitehall_formats
+    Artefact::FORMATS_BY_DEFAULT_OWNING_APP.each_with_object([]) do |(owning_app, formats), result|
+      if owning_app != "whitehall"
+        result.concat(formats)
+      end
+    end
+  end
 end
