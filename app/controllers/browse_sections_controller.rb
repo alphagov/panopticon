e589bbf74fcc74194f414b2ee75276fc655406f4
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-03-04 14:53:09 +0000

    Add basic edit form for browse sections (tags)

--- app/controllers/browse_sections_controller.rb
+++ app/controllers/browse_sections_controller.rb
@@ -1,12 +1,30 @@
 class BrowseSectionsController < ApplicationController
   before_filter :require_permission
+  before_filter :find_section, only: [:edit, :update]
 
   def index
     @sections = Tag.where(tag_type: "section").order_by([:tag_id, :asc])
   end
 
+  def edit
+  end
+
+  def update
+    clean_params = params[:section].delete_if { |k,v| k == "tag_id" }
+    if @section.update_attributes(clean_params)
+      redirect_to browse_sections_path, notice: "Updated #{@section.title} browse section"
+    else
+      flash[:error] = "Failed to save"
+      render :edit
+    end
+  end
+
   private
     def require_permission
       authorise_user!("Browse section admin")
+    end
+
+    def find_section
+      @section = Tag.find(params[:id])
     end
 end
--- test/functional/browse_sections_controller_test.rb
+++ test/functional/browse_sections_controller_test.rb
@@ -29,4 +29,33 @@ class BrowseSectionsControllerTest < ActionController::TestCase
       assert_select "tbody tr", 2
     end
   end
+
+  context "GET edit" do
+    should "show the artefacts"
+  end
+
+  context "PUT update" do
+    setup do
+      login_as_user_with_permission
+      @section = FactoryGirl.create(:tag, tag_type: "section", tag_id: "a", title: "A")
+    end
+
+    should "update the section tag" do
+      put :update, id: @section.id, section: { title: "Amore" }
+      assert_equal "Amore", @section.reload.title
+      assert_redirected_to browse_sections_path
+      assert_match(/updated/i, flash[:notice])
+    end
+
+    should "rerender the form if invalid" do
+      put :update, id: @section.id, section: { title: "" }
+      assert_template :edit
+      assert_match(/Failed to save/i, flash[:error])
+    end
+
+    should "prevent updates to the tag_id (slug)" do
+      put :update, id: @section.id, section: { tag_id: "h4x0r" }
+      assert_equal "a", @section.reload.tag_id
+    end
+  end
 end
