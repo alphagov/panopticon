40df976f7145599dc670dc0153d81b60f9c47333
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-03-04 12:48:37 +0000

    Restrict access to users with permission

--- app/controllers/browse_sections_controller.rb
+++ app/controllers/browse_sections_controller.rb
@@ -1,5 +1,12 @@
 class BrowseSectionsController < ApplicationController
+  before_filter :require_permission
+
   def index
     @sections = Tag.where(tag_type: "section").order_by([:tag_id, :asc])
   end
+
+  private
+    def require_permission
+      authorise_user!("Browse section admin")
+    end
 end
--- test/functional/browse_sections_controller_test.rb
+++ test/functional/browse_sections_controller_test.rb
@@ -1,12 +1,25 @@
 require 'test_helper'
 
 class BrowseSectionsControllerTest < ActionController::TestCase
-  setup do
-    login_as_stub_user
+
+  def login_as_user_with_permission
+    u = stub_user
+    u.permissions = u.permissions + ["Browse section admin"]
+    u.save!
+    login_as(u)
+  end
+
+  context "access control" do
+    should "only grant access to users with permission" do
+      login_as_stub_user
+      get :index
+      assert_response 403
+    end
   end
 
   context "GET index" do
     setup do
+      login_as_user_with_permission
       FactoryGirl.create(:tag, tag_type: "section", tag_id: "a", title: "A")
       FactoryGirl.create(:tag, tag_type: "section", tag_id: "a/1", title: "1", parent_id: "a")
     end
