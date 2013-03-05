9e67464f679f0d795e42a0c0c14ef2470f9e8da7
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-03-05 10:29:53 +0000

    Remove Tag UI and API
    The UI was useful for editing sections, the API was used by Frontend/Rummager
    back in the day to render browse pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Panopticon::Application.routes.draw do
   resources :artefacts, :constraints => { :id => /[^\.]+/ }
   resources :browse_sections
-  resources :tags
 
-  match 'tags/:id' => 'tags#show', :id =>  /[^\.]+/, :defaults => {:format => 'json'}
-  match 'categories' => 'tags#categories', :as => :categories
   match 'google_insight' => 'seo#show'
   root :to => redirect("/artefacts")
 end
