ba11bebfcf2cf06c830de4828b216989da59302a
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-03-20 15:40:28 +0000

    Reinstate sorting on the artefacts index

--- app/controllers/artefacts_controller.rb
+++ app/controllers/artefacts_controller.rb
@@ -3,6 +3,7 @@ class ArtefactsController < ApplicationController
   before_filter :build_artefact, :only => [:new, :create]
   before_filter :tag_collection, :except => [:show]
   helper_method :relatable_items
+  helper_method :sort_column, :sort_direction
 
   respond_to :html, :json
 
@@ -14,10 +15,11 @@ class ArtefactsController < ApplicationController
       tags = Tag.where(tag_type: "section", parent_id: @section)
       tag_ids = tags.collect {|t| t.tag_id}
       tag_ids << @section
-      @artefacts = Artefact.any_in(tag_ids: tag_ids).order_by([[:name, :asc]])
+      @artefacts = Artefact.any_in(tag_ids: tag_ids)
     else
-      @artefacts = Artefact.order_by([[:name, :asc]])
+      @artefacts = Artefact
     end
+    @artefacts = @artefacts.order_by([[sort_column, sort_direction]])
     @artefacts = @artefacts.page(params[:page]).per(ITEMS_PER_PAGE)
     respond_with @artefacts, @tag_collection
   end
@@ -185,5 +187,13 @@ class ArtefactsController < ApplicationController
       (reverse_actions + [nil]).each_cons(2).map { |action, previous|
         DiffEnabledAction.new(action, previous)
       }
+    end
+
+    def sort_column
+      Artefact.fields.keys.include?(params[:sort]) ? params[:sort] : :name
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : :asc
     end
 end
