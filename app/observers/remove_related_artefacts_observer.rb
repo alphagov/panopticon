e7e70747d4a934409d2dfcdfa4217ee3b7a52c5a
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-22 12:04:04 +0000

    Use the archived? helper method instead of accessing the field
    This helper method already exists and is being used across the app. Be
    consistent and use it instead of differences between this and the
    state field equality check.

--- app/observers/remove_related_artefacts_observer.rb
+++ app/observers/remove_related_artefacts_observer.rb
@@ -2,7 +2,7 @@ class RemoveRelatedArtefactsObserver < Mongoid::Observer
   observe :artefact
 
   def after_save(artefact)
-    if artefact.state == 'archived'
+    if artefact.archived?
       Artefact.where(:related_artefact_ids.in => [artefact.id]).each do | a |
         a.related_artefact_ids.delete(artefact.id)
         a.save
