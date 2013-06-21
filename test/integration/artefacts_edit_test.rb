c5ae06a085e44e1286a9e091f20bf502f39d31ed
Jack Franklin <jack@jackfranklin.net>
2013-06-21 11:39:27 +0100

    Update the integration test to not rely on the presentation class name

--- test/integration/artefacts_edit_test.rb
+++ test/integration/artefacts_edit_test.rb
@@ -134,7 +134,7 @@ class ArtefactsEditTest < ActionDispatch::IntegrationTest
       visit "/artefacts"
       click_on "Alpha"
       click_on "Add related external link"
-      within ".related-external-links .row:first-child" do
+      within ".related-external-links div:first-child" do
         fill_in "Title", :with => "BBC"
         fill_in "URL", :with => "http://bbc.co.uk"
       end
