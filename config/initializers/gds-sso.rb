8a595df2b225ff25600fb6cfe80aad96faca7958
pezholio <pezholio@gmail.com>
2013-07-17 17:46:42 +0100

    Add a sensible default in ENV not present

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,8 +1,8 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV['PANOPTICON_OAUTH_ID']
-  config.oauth_secret = ENV['PANOPTICON_OAUTH_SECRET']
+  config.oauth_id     = ENV['PANOPTICON_OAUTH_ID'] || "abcdefgh12345678pan"
+  config.oauth_secret = ENV['PANOPTICON_OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.current.find("signon")
-  config.basic_auth_user = ENV['PANOPTICON_USER']
-  config.basic_auth_password = ENV['PANOPTICON_PASSWORD']
+  config.basic_auth_user = ENV['PANOPTICON_USER'] || "api"
+  config.basic_auth_password = ENV['PANOPTICON_PASSWORD'] || "defined_on_rollout_not"
 end
