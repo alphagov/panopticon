7264e47c46d08769033a19e73266336ad85569b3
pezholio <pezholio@gmail.com>
2013-07-17 13:46:12 +0100

    Load OAuth and basic auth details from ENV

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,8 +1,8 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = 'abcdefgh12345678pan'
-  config.oauth_secret = 'secret'
+  config.oauth_id     = ENV['PANOPTICON_OAUTH_ID']
+  config.oauth_secret = ENV['PANOPTICON_OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
-  config.basic_auth_user = 'api'
-  config.basic_auth_password = 'defined_on_rollout_not'
+  config.basic_auth_user = ENV['PANOPTICON_USER']
+  config.basic_auth_password = ENV['PANOPTICON_PASSWORD']
 end
