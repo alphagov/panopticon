ba3d22f9f14af5f99bbb2ec0d809d06da848a766
pezholio <pezholio@gmail.com>
2013-07-26 10:49:26 +0100

    Add tags

--- db/seeds/tags.rb
+++ db/seeds/tags.rb
@@ -5,20 +5,60 @@ def create_or_update_tag(options)
 end
 
 create_or_update_tag(
+   tag_type: "section",
+   title: "Global",
+   tag_id: "global",
+   description: "Generic content that should be pulled onto the global site")
+
+create_or_update_tag(
+   tag_type: "section",
+   title: "London",
+   tag_id: "london",
+   description: "Generic content that should be pulled onto the London site")
+
+create_or_update_tag(
+   tag_type: "section",
+   title: "Learning",
+   tag_id: "learning",
+   description: "Generic content that should be pulled onto the learning site")
+     
+create_or_update_tag(
     tag_type: "section",
-    title: "Global",
-    tag_id: "global",
-    description: "Generic content that should be pulled onto the global site")
+    title: "People",
+    tag_id: "people",
+    description: "Generic content that should be pulled onto the people site")
 
 create_or_update_tag(
     tag_type: "section",
-    title: "London",
-    tag_id: "london",
-    description: "Generic content that should be pulled onto the London site")
+    title: "Tech Team",
+    tag_id: "people/tech-team",
+    parent_id: "people",
+    description: "People in the tech team")
 
 create_or_update_tag(
     tag_type: "section",
-    title: "Learning",
-    tag_id: "learning",
-    description: "Generic content that should be pulled onto the learning site")
+    title: "Commercial Team",
+    tag_id: "people/commercial-team",
+    parent_id: "people",
+    description: "People in the commercial team")
+    
+create_or_update_tag(
+    tag_type: "section",
+    title: "Executive Team",
+    tag_id: "people/executive-team",
+    parent_id: "people",
+    description: "People in the executive team")
 
+create_or_update_tag(
+    tag_type: "section",
+    title: "Board",
+    tag_id: "people/board",
+    parent_id: "people",
+    description: "People on the board")
+    
+create_or_update_tag(
+    tag_type: "section",
+    title: "Operations Team",
+    tag_id: "people/operations-team",
+    parent_id: "people",
+    description: "People in the operations team")
