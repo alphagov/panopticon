68542b5243c054abfc8285b3dddd71a2f2431a6a
James Smith <james@floppy.org.uk>
2013-08-22 14:45:35 +0100

    Allow disabling of search in production mode using env var

--- config/initializers/search_callback.rb
+++ config/initializers/search_callback.rb
@@ -1,7 +1,7 @@
 # In development environments we don't want to depend on Rummager unless
 # explicitly told to do so
 unless Rails.env.development?
-  update_search = true
+  update_search = ENV['QUIRKAFLEEG_SEARCH_DISABLE'].nil?
 else
   update_search = ENV['UPDATE_SEARCH'].present?
 end
