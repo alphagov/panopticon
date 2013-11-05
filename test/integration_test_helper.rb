17acf73fc67bd9449b7cd9eaf119ca78062905ce
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-11-05 14:55:18 +0000

    Integration tests for router registraion

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -10,5 +10,15 @@ class ActionDispatch::IntegrationTest
   teardown do
     Capybara.use_default_driver
   end
+
+  def put_json(path, attrs, headers = {})
+    # Rack::Test put method calls to_json on whatever body you pass.
+    # This is different to the post method.  Go figure.
+    put path, attrs, {"Content-Type" => "application/json"}.merge(headers)
+  end
+
+  def stub_all_rummager_requests
+    WebMock.stub_request(:any, %r{\A#{Rummageable.rummager_host}})
+  end
 end
 
