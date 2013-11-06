8844728e660b3b52bbe2286fe8561f4bdcb6a5f8
Jordan Hatch <jordan@jordanh.net>
2013-11-06 13:45:10 +0000

    Add link_to_view_need helper
    This takes an artefact and renders a link to view
    the need in the correct service. 
    
    In the case where there isn't an appropriate service
    to link to (eg if the need ID is a string), we
    don't show any link at all.

--- test/unit/helpers/artefacts_helper_test.rb
+++ test/unit/helpers/artefacts_helper_test.rb
@@ -16,4 +16,20 @@ class ArtefactsHelperTest < ActionView::TestCase
       assert_nil need_url(artefact)
     end
   end
+
+  context "link_to_view_need" do
+    should "render a link to the need" do
+      artefact = OpenStruct.new(need_owning_service: "needorama")
+      expects(:need_url).with(artefact).returns("http://needorama.dev.gov.uk/a-need")
+
+      expected = "<a href=\"http://needorama.dev.gov.uk/a-need\" class=\"btn btn-primary\" rel=\"external\">View in Needorama</a>"
+      assert_equal expected, link_to_view_need(artefact)
+    end
+
+    should "not render a link if need_owning_service is nil" do
+      artefact = OpenStruct.new(need_owning_service: nil)
+
+      assert_nil link_to_view_need(artefact)
+    end
+  end
 end
