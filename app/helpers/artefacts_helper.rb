3f816d80beddd409166299fe8c116f24de7597ae
Jordan Hatch <jordan@jordanh.net>
2013-11-06 13:43:43 +0000

    Change the need_url helper to use the owning service
    Use the owning service to build the host, based on
    the return value of the need_owning_service method
    on the method argument.
    
    Both Maslow and the Need-o-tron provide an endpoint
    to access needs at /needs/X

--- app/helpers/artefacts_helper.rb
+++ app/helpers/artefacts_helper.rb
@@ -1,8 +1,4 @@
 module ArtefactsHelper
-  def need_url(artefact)
-    Plek.current.find('needotron') + "/needs/#{artefact.need_id}"
-  end
-
   def published_url(artefact)
     Plek.current.website_root + "/#{artefact.slug}"
   end
@@ -21,5 +17,11 @@ module ArtefactsHelper
         result.concat(formats)
       end
     end
+  end
+
+  def need_url(artefact)
+    return unless artefact.need_owning_service
+
+    Plek.current.find(artefact.need_owning_service) + "/needs/#{artefact.need_id}"
   end
 end
