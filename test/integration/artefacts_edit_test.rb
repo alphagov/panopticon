ceb6c6f351b0225e71b170a013f071f799de4346
pezholio <pezholio@gmail.com>
2013-12-04 17:42:29 +0000

    Test for presence of both legacy_sources, but don't worry about the order

--- test/integration/artefacts_edit_test.rb
+++ test/integration/artefacts_edit_test.rb
@@ -67,7 +67,9 @@ class ArtefactsEditTest < ActionDispatch::IntegrationTest
       click_on "Save and continue editing"
 
       @a.reload
-      assert_equal [@dvla, @bl], @a.legacy_sources
+      assert_equal 2, @a.legacy_sources.count
+      assert @a.legacy_sources.include?(@dvla)
+      assert @a.legacy_sources.include?(@bl)
     end
 
     should "allow removing legacy sources from artefacts" do
