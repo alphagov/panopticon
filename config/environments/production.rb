3aabce71dad472f284399b8fa285e1f079b9c940
Vinay Patel <mail@vinayvinay.com>
2013-12-18 14:52:12 +0000

    Removing references to lograge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,4 @@ Panopticon::Application.configure do
 
   config.action_mailer.default_url_options = { :host => Plek.current.find('panopticon') }
   config.action_mailer.delivery_method = :ses
-
-  config.lograge.enabled = true
 end
