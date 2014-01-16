d497bdcfab4525abe09b11d9cef32af62098c05f
Jordan Hatch <jordan@jordanh.net>
2014-01-16 15:42:02 +0000

    Add an integration test for filtering by section

--- test/integration/artefacts_index_test.rb
+++ test/integration/artefacts_index_test.rb
@@ -30,4 +30,42 @@ class ArtefactsIndexTest < ActionDispatch::IntegrationTest
     assert alpha.has_content?("en")
     assert bravo.has_content?("cy")
   end
+
+  context "filtering artefacts" do
+    should "filter by section" do
+      FactoryGirl.create(:tag, tag_id: 'driving', tag_type: 'section', title: 'Driving')
+      FactoryGirl.create(:tag, tag_id: 'driving/learning-to-drive', tag_type: 'section', title: 'Learning to drive', parent_id: 'driving')
+
+      FactoryGirl.create(:artefact, name: 'VAT rates', slug: 'vat-rates', section_ids: [])
+      FactoryGirl.create(:artefact, name: 'Guide to driving', slug: 'guide-to-driving', section_ids: ['driving'])
+      FactoryGirl.create(:artefact, name: 'Book driving test', slug: 'book-driving-test', section_ids: ['driving/learning-to-drive'])
+
+      visit '/artefacts'
+
+      within "#artefact-list" do
+        assert page.has_selector?("tr", text: "VAT rates")
+        assert page.has_selector?("tr", text: "Guide to driving")
+        assert page.has_selector?("tr", text: "Book driving test")
+      end
+
+      within ".sidebar-nav ul" do
+        assert page.has_selector?("li.active", text: "All")
+        assert page.has_selector?("li:not(.active)", text: "Driving")
+
+        click_on "Driving"
+      end
+
+      within "#artefact-list" do
+        assert page.has_selector?("tr", text: "Guide to driving")
+        assert page.has_selector?("tr", text: "Book driving test")
+
+        assert page.has_no_selector?("tr", text: "VAT rates")
+      end
+
+      within ".sidebar-nav ul" do
+        assert page.has_selector?("li:not(.active)", text: "All")
+        assert page.has_selector?("li.active", text: "Driving")
+      end
+    end
+  end
 end
