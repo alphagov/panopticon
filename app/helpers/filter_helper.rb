32d210ec73de914e178827917ff722e27b63fc16
Jordan Hatch <jordan@jordanh.net>
2014-01-17 15:58:41 +0000

    Fix the selected value of filter box when blank
    When there's no filter applied, the select box should explicitly mark 
    the "All" option as "selected".

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -3,26 +3,26 @@ module FilterHelper
     tags = Tag.where(tag_type: tag_type, parent_id: nil)
 
     tags_with_labels = tags.map {|tag| [tag.title, tag.tag_id] }
-    options = [["All", nil]] + tags_with_labels
+    options = [["All", ""]] + tags_with_labels
 
-    options_for_select(options, selected_value)
+    options_for_select(options, selected_value || "")
   end
 
   def options_from_formats_for_select(selected_value=nil)
     formats_with_labels = Artefact::FORMATS.sort.map {|format|
       [ format.underscore.humanize, format ]
     }
-    options = [["All", nil]] + formats_with_labels
+    options = [["All", ""]] + formats_with_labels
 
-    options_for_select(options, selected_value)
+    options_for_select(options, selected_value || "")
   end
 
   def options_from_states_for_select(selected_value=nil)
     states_with_labels = Artefact::STATES.sort.map {|state|
       [ state.humanize, state ]
     }
-    options = [["All", nil]] + states_with_labels
+    options = [["All", ""]] + states_with_labels
 
-    options_for_select(options, selected_value)
+    options_for_select(options, selected_value || "")
   end
 end
