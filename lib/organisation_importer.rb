2f37e83f8ca37dd195799266c3fb083d2eb9fc84
Jordan Hatch <jordan@jordanh.net>
2014-03-05 11:58:20 +0000

    Add an organisation importer
    This adds a new class OrganisationImporter. It requests the list of all 
    organisations from the API, and iterates over each result. If a tag 
    doesn't already exist for the organisation, one will be created. If a 
    tag already exists, then the tag title will be updated only if the 
    organisation name has changed.
    
    The importer is invoked through a new Rake task: organisations:import

---
+++ lib/organisation_importer.rb
@@ -0,0 +1,76 @@
+require 'gds_api/organisations'
+
+class OrganisationImporter
+  TAG_TYPE = "organisation"
+
+  def run
+    logger.info "Fetching all organisations from the Organisation API"
+    organisations = organisations_api.organisations.with_subsequent_pages.to_a
+    logger.info "Loaded #{organisations.size} organisations"
+
+    organisations.each do |organisation|
+      create_or_update_organisation(organisation)
+    end
+
+    logger.info "Import complete"
+  end
+
+  private
+
+  def create_or_update_organisation(organisation)
+    tag_id = organisation.details.slug
+    existing_tag = Tag.where(tag_type: TAG_TYPE, tag_id: tag_id).first
+
+    if existing_tag.present?
+      if existing_tag.title != organisation.title
+        if existing_tag.update_attributes(title: organisation.title)
+          logger.info "Found existing tag: #{tag_id}; updated title to '#{existing_tag.title}'"
+        else
+          logger.info "Found existing tag: #{tag_id}; could not update title: #{existing_tag.errors.full_messages.join(', ')}"
+        end
+      else
+        logger.info "Found existing tag: #{tag_id}"
+      end
+    else
+      new_tag = Tag.new(tag_type: TAG_TYPE,
+                        tag_id: tag_id,
+                        title: organisation.title)
+
+      if new_tag.save
+        logger.info "Created organisation tag: #{tag_id}"
+      else
+        logger.info "Could not create organisation tag: #{tag_id} - #{new_tag.errors.full_messages.join(', ')}"
+      end
+    end
+  end
+
+  def logger
+    @logger ||= build_logger
+  end
+
+  def build_logger
+    output = case Rails.env
+             when "development" then STDOUT
+             when "test" then "/dev/null"
+             when "production" then Rails.root.join("log", "organisation_import.json.log")
+             end
+
+    Logger.new(output).tap {|logger|
+      logger.formatter = json_log_formatter if Rails.env.production?
+    }
+  end
+
+  def json_log_formatter
+    proc {|severity, datetime, progname, message|
+      {
+        "@message" => message,
+        "@tags" => ["cron", "rake"],
+        "@timestamp" => datetime.iso8601
+      }.to_json + "\n"
+    }
+  end
+
+  def organisations_api
+    @api_client ||= GdsApi::Organisations.new(Plek.current.find('whitehall-admin'))
+  end
+end
