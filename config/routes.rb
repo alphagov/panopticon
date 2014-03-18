c74f75cd79d037fdfee3de7aa935f6cef026bfc5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-03-18 09:30:45 +0000

    Allow artefact registration to support whitehall slugs
    This needs to support slugs with consecutive '-'s in them, and
    aditionally some with a .locale suffix.
    
    The former is addressed by the content_models bump, whereas the latter
    additionally needed support in the routes.
    
    Fixes [#67540714, #67541692]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Panopticon::Application.routes.draw do
-  resources :artefacts, :constraints => { :id => /[^\.]+/ } do
+  # This is necessary to support some whitehall artefacts with a locale extension on the end
+  # of the slug.  If we just blanket allow . in the slug, this interferes with the formatted
+  # routes, so this instead special cases the 3 variants of locale extensions (.fr, .zh-hk, .es-419)
+  artefact_id_regex = %r{
+    [^\.]+                # 1 or more non-dot characters
+    (\.[a-z]{2}           # optionally dot followed by a basic locale (eg 'fr')
+      (                     # optionally followed by a locale extension
+        -[a-z]{2} |           # eg zh-hk
+        -\d{3}                # eg es-419
+      )?
+    )?
+  }x
+  resources :artefacts, :constraints => { :id => artefact_id_regex } do
     member do
       get :history
       get :archive
