c763b7a0dd5dd28c707c2712468f81df1d36375a
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-04-08 10:01:12 +0000

    Maslow need id tags are links to the need in maslow

--- app/assets/javascripts/views/artefacts_edit.js
+++ app/assets/javascripts/views/artefacts_edit.js
@@ -1,13 +1,34 @@
 $(function() {
   $(document).ready(function() {
-    var needIdsSelect = $(".js-artefact-need-ids");
-    needIdsSelect.tagsinput();
+    var $needIdsSelect = $(".js-artefact-need-ids"),
+        maslowNeedsUrl = $("#js-maslow-needs-url").text(),
+        needotronNeedsUrl = $("#js-needotron-needs-url").text();
 
-    var needIdTagInputField = $("div.bootstrap-tagsinput input[type=text]")
-    needIdTagInputField.mask("999999");
+    $needIdsSelect.tagsinput();
+
+    // need the id for easier javascript integration tests
+    var tagsInputTextbox = $needIdsSelect.tagsinput("input");
+    $(tagsInputTextbox)
+      .attr("id", "tagsinput-text-box")
+      .attr("size", "6");
+
+    var $needIdTagInputField = $("div.bootstrap-tagsinput input[type=text]");
+    $needIdTagInputField.mask("999999");
+
     $(".js-add-artefact-need-id").live("click", function() {
-      needIdsSelect.tagsinput("add", needIdTagInputField.val());
-      needIdTagInputField.val("").focus();
+      $needIdsSelect.tagsinput("add", $needIdTagInputField.val());
+      $needIdTagInputField.val("").focus();
+    });
+
+    $("#artefact_need_ids_input .tag").live("click", function(e) {
+      var needId = $(this).text(),
+          sixDigitIntegerMatcherRegex = /^\d{6}$/;
+
+      if(sixDigitIntegerMatcherRegex.test(needId)) {
+        window.open(maslowNeedsUrl + needId);
+      } else {
+        window.open(needotronNeedsUrl + needId);
+      }
     });
   });
 });
