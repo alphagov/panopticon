a07e880e3b613089367eec98849313a734a213d4
pezholio <pezholio@gmail.com>
2014-04-24 13:05:34 +0100

    Last fail (I hope)

--- test/unit/search_index_test.rb
+++ test/unit/search_index_test.rb
@@ -4,7 +4,7 @@ class SearchIndexTest < ActiveSupport::TestCase
 
   test "builds a Rummageable::Index instance" do
     Rummageable::Index.expects(:new)
-                        .with('http://search.dev', '/mainstream', has_key(:logger))
+                        .with('http://search.dev', '/dapaas', has_key(:logger))
                         .returns('search client')
 
     client = SearchIndex.instance
