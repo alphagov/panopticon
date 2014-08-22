85d42049c45a7b6e4989da09b7568e30eeced27b
Paul Hayes <fofr@users.noreply.github.com>
2014-08-22 15:33:01 +0100

    Fix display of slug in new tag form
    * Use form_control_input_html_options so text input has correct
    bootstrap classes
    * Rather than generating HTML to prepend, use the built-in prepend
    method, and merge this with existing input options. See:
    https://github.com/justinfrench/formtastic#creating-new-inputs-based-on-
    existing-ones

--- app/inputs/child_tag_id_input.rb
+++ app/inputs/child_tag_id_input.rb
@@ -1,22 +1,19 @@
 class ChildTagIdInput < FormtasticBootstrap::Inputs::StringInput
+
+  def options
+    if builder.object.parent_id.present?
+      super.merge(:prepend => "#{builder.object.parent_id}/")
+    end
+  end
+
   def to_html
     remove_parent_from_tag_id
-
     bootstrap_wrapping do
-      parent_tag_html <<
-      builder.text_field(method, input_html_options)
+      builder.text_field(method, form_control_input_html_options)
     end
   end
 
   protected
-
-  def parent_tag_html
-    if builder.object.parent_id.present?
-      builder.template.content_tag :span, class: "parent-id" do
-        "#{builder.object.parent_id}/"
-      end
-    end
-  end
 
   def remove_parent_from_tag_id
     if builder.object.tag_id.present?
