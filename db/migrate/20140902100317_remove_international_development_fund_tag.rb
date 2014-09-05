fcfb41020575de3a7fde60e4dcd31c1296fd3142
Tom Russell <tom@cishub.co.uk>
2014-09-05 12:04:17 +0100

    Notify the router API to add redirects when the migration runs

--- db/migrate/20140902100317_remove_international_development_fund_tag.rb
+++ db/migrate/20140902100317_remove_international_development_fund_tag.rb
@@ -1,6 +1,14 @@
+require "gds_api/router"
+
 class RemoveInternationalDevelopmentFundTag < Mongoid::Migration
+  def self.router_api
+    @router_api ||= GdsApi::Router.new(Plek.current.find('router-api'))
+  end
+
   def self.up
     Tag.by_tag_id("citizenship/international-development", "section").destroy
+    router_api.add_redirect_route('/browse/citizenship/international-development', 'exact', '/international-development-funding')
+    router_api.commit_routes
   end
 
   def self.down
