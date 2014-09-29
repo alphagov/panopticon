102d99d0267817cae66a89a9789afc316f235ad2
pezholio <pezholio@gmail.com>
2014-09-29 16:14:03 +0100

    Remove line numbers

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
 if ENV['QUIRKAFLEEG_AIRBRAKE_KEY']
-2	  Airbrake.configure do |config|
-3	    config.api_key = ENV['QUIRKAFLEEG_AIRBRAKE_KEY']
-4	  end
-5	end
+  Airbrake.configure do |config|
+    config.api_key = ENV['QUIRKAFLEEG_AIRBRAKE_KEY']
+  end
+end
