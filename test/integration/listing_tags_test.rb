9aa1043394e5d80f75d49f801b35d01d8bb77174
Kushal Pisavadia <kushi.p@gmail.com>
2015-03-25 15:54:05 +0000

    When listing tags, provide show over edit links

--- test/integration/listing_tags_test.rb
+++ test/integration/listing_tags_test.rb
@@ -20,29 +20,19 @@ class ListingTagsTest < ActionDispatch::IntegrationTest
       within "li.parent-tag:nth-of-type(1)" do
         assert page.has_content?('Section: Business')
 
-        assert page.has_link?('Edit tag', href: edit_tag_path(business))
-        assert page.has_link?('Add child tag', href: new_tag_path(type: 'section', parent_id: 'business'))
-
         within ".children li:nth-of-type(1)" do
           assert page.has_content?('Manufacturing')
           assert page.has_link?('business/manufacturing', href: "http://www.dev.gov.uk/browse/business/manufacturing")
-
-          assert page.has_link?('Edit tag', href: edit_tag_path(children[1]))
         end
 
         within ".children li:nth-of-type(2)" do
           assert page.has_content?('Setting up')
           assert page.has_link?('business/setting-up', href: "http://www.dev.gov.uk/browse/business/setting-up")
-
-          assert page.has_link?('Edit tag', href: edit_tag_path(children[0]))
         end
       end
 
       within "li.parent-tag:nth-of-type(2)" do
         assert page.has_content?('Section: Driving')
-
-        assert page.has_link?('Edit tag', href: edit_tag_path(driving))
-        assert page.has_link?('Add child tag', href: new_tag_path(type: 'section', parent_id: 'driving'))
       end
     end
   end
