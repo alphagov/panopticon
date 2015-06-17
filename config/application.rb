593e8dce54df77aa20ea94729e77d39493370581
Stuart Gale <bishboria@gmail.com>
2015-06-17 12:55:04 +0100

    Version bump of admin and chosen-rails gems
    Due to a vulnerability with jquery-rails. See See
    https://groups.google.com/forum/#!topic/rubyonrails-security/XIZPbobuwaY
    for details
    
    * admin gem deals with Analytics
    * Change to config/application.rb is a reported issue rails/sass-rails#315
    * chosen-rails interacts with sass so have to upgrade it too

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
+require "sass"
 require "sprockets/railtie"
 require 'kaminari' # has to be loaded before the models, otherwise the methods aren't added
 require "govuk_content_models"
