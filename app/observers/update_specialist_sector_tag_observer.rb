8e9b52652db3f6a40ea60857caaa38f0f2be4304
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-26 13:39:26 +0000

    Remove updating of artefacts from tag observer
    These artefacts will no longer exist.
    
    Also rename the observer to better reflect what it does.

--- app/observers/update_specialist_sector_tag_observer.rb
+++ app/observers/update_specialist_sector_tag_observer.rb
@@ -1,9 +1,8 @@
-class UpdateSpecialistSectorTagArtefactObserver < Mongoid::Observer
+class UpdateSpecialistSectorTagObserver < Mongoid::Observer
   observe :tag
 
   def after_update(tag)
     if tag.tag_type == 'specialist_sector'
-      update_artefact(tag)
       reindex_tagged_documents(tag) if tag.state_changed? && tag.live?
     end
   end
@@ -13,10 +12,5 @@ private
   def reindex_tagged_documents(tag)
     Panopticon.whitehall_admin_api.reindex_specialist_sector_editions(tag.tag_id)
     Panopticon.publisher_api.reindex_topic_editions(tag.tag_id)
-  end
-
-  def update_artefact(tag)
-    artefact = Artefact.where(kind: 'specialist_sector', slug: tag.tag_id).first
-    artefact.update_attributes(name: tag.title, state: tag.state) if artefact
   end
 end
---
+++ test/unit/observers/update_specialist_sector_tag_observer_test.rb
@@ -0,0 +1,28 @@
+require_relative '../../test_helper'
+
+class UpdateSpecialistSectorTagObserverTest < ActiveSupport::TestCase
+  setup do
+    Panopticon.whitehall_admin_api.stubs(reindex_specialist_sector_editions: nil)
+    Panopticon.publisher_api.stubs(reindex_topic_editions: nil)
+
+    @tag = FactoryGirl.create(:draft_tag,
+      tag_type: 'specialist_sector',
+      tag_id: 'oil-and-gas/licensing',
+      parent_id: 'oil-and-gas'
+    )
+  end
+
+  should 'trigger reindex of tagged documents on publish' do
+    Panopticon.whitehall_admin_api.expects(:reindex_specialist_sector_editions).with('oil-and-gas/licensing').once
+    Panopticon.publisher_api.expects(:reindex_topic_editions).with('oil-and-gas/licensing').once
+
+    @tag.publish!
+  end
+
+  should 'not trigger reindex of tagged documents on non-publish saves' do
+    Panopticon.whitehall_admin_api.expects(:reindex_specialist_sector_editions).never
+    Panopticon.publisher_api.expects(:reindex_topic_editions).never
+
+    @tag.update_attributes(title: 'A brand new title')
+  end
+end
