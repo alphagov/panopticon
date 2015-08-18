e54f859ec80f45ea91f3794708fa938209bac462
Paul Bowsher <paul.bowsher@gmail.com>
2015-08-18 11:16:24 +0100

    Convert long-running migration into script

---
+++ script/correct_routes_for_detailed_guides.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/env ruby
+
+unless defined? Rails
+  $stderr.puts "Run me with script/rails r #$0"
+  exit 1
+end
+
+require "gds_api/router"
+
+router_api = GdsApi::Router.new(Plek.find("router-api"))
+
+scope = Artefact.where(kind: "detailed_guide")
+count = scope.count
+scope.each_with_index do |guide, i|
+  puts "Processing #{guide.slug} (#{i + 1}/#{count})"
+  guide.slug.sub!(%r{^(guidance/)?(deleted-)?}, 'guidance/')
+
+  # Check the root route is owned by Whitehall
+  route = router_api.get_route("/#{guide.slug.sub(%r{^guidance/}, '')}")
+  if route && route["handler"] == "backend" && route["backend_id"] == "whitehall-frontend"
+    guide.paths = guide.paths.map { |pa| pa.sub(%r{^/(guidance/)?(deleted-)?}, '/guidance/') }.uniq
+    puts "Paths set to #{guide.paths.inspect}"
+  else
+    puts "Skipping, not owned by whitehall"
+  end
+
+  guide.save
+end
