7c9809191392e1cc5eafb17290b9c0a21d48e151
Tijmen Brommet <tijmen@gmail.com>
2015-11-19 12:34:04 +0000

    Set parent link as primary section
    We are splitting up the mainstream_browse_page into `parent` (used for
    breadcrumb) and the main tags.
    
    This commit makes sure that if we send a `parent` link, we copy that
    tag to the front of the section tags. This will ensure that the same
    browse page will be displayed in the breadcrumb on mainstream browse
    pages.

--- app/queue_consumers/tagging_updater.rb
+++ app/queue_consumers/tagging_updater.rb
@@ -46,6 +46,11 @@ private
       artefact.set_tags_of_type(panopticon_tag_name, new_tags.map(&:tag_id))
     end
 
+    if content_item['links']['parent']
+      parent = Tag.where(:content_id.in => content_item['links']['parent'])
+      artefact.set_primary_tag_of_type('section', parent.first.tag_id)
+    end
+
     artefact.save!
   end
 end
