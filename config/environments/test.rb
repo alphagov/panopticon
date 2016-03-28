1fb952d6f19588648ba1597d11a0d40669891a7f
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-03-28 21:44:44 +0000

    Update configuration for new versions.
    This will need changes to puppet to add the SECRET_KEY_BASE and MONGODB_URI
    env vars.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,8 +7,13 @@ Panopticon::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
@@ -34,4 +39,6 @@ Panopticon::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  config.active_support.test_order = :random
 end
