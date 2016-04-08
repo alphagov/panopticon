5f29f26f556f2c4dde9aa64284a84b4dc95a6fa1
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-04-08 16:03:08 +0000

    Update list of non migrated apps
    This commit does several things:
    
    - Removes config gem
    - Removes config initializer that reads the settings.yml file
    - Removes settings.yml file
    - uses NON_MIGRATED_APPS constant in Artefact object instead
    
    All this changes aim to ease future changes, since migrating apps to the
    new tagging is a work in progress at the moment.
    
    This NON_MIGRATED_APPS constant should be in sync with
    [Content
    Tagger](https://github.com/alphagov/content-tagger)
    [blacklisted-apps](https://github.com/alphagov/content-tagger/blob/master/config/blacklisted-apps.yml)
    
    And with [Rummanger](https://github.com/alphagov/rummager)
    [NON_MIGRATED_APPS](https://github.com/alphagov/rummager/blob/master/lib/indexer/index_documents.rb#L6)
    constant.
    
    By removing the config gem we were able to see that  TaggingUpdater was
    stil coupled with the settings.yml file, if we had not removed it this
    would had reached production with a silent bug that was not catched by
    the test suite.
    
    Part of:
    https://trello.com/c/tZd23gW5/568-allow-tagging-in-content-tagger-for-apps-that-can-t-be-tagged-yet-s

--- test/unit/artefact/artefact_test.rb
+++ test/unit/artefact/artefact_test.rb
@@ -2,23 +2,6 @@ require_relative '../../test_helper'
 
 class Artefact::ArtefactTest < ActiveSupport::TestCase
 
-  context "app_without_tagging?" do
-    context "when artefact is from a deprecated application" do
-
-      should "return true when owning app is in the apps_without_migrated_tagging" do
-        artefact = FactoryGirl.create(:artefact, slug: "low-hanging-fruit", owning_app: 'finder-api')
-
-        assert_equal true, artefact.app_without_tagging?
-      end
-
-      should "return false when owning app is not in the apps_without_migrated_tagging" do
-        artefact = FactoryGirl.create(:artefact, slug: "low-hanging-fruit", owning_app: 'whitehall')
-
-        assert_equal false, artefact.app_without_tagging?
-      end
-    end
-  end
-
   context "tagging_migrated?" do
 
     context 'standard set of untaggable apps (publisher, smartanswers and testapp)' do
@@ -35,24 +18,16 @@ class Artefact::ArtefactTest < ActiveSupport::TestCase
 
       should 'return false if new record and owning app is nil' do
         artefact = Artefact.new
-        assert_equal true, artefact.tagging_migrated?
+        assert_equal false, artefact.tagging_migrated?
       end
 
-      should 'return true if new record and owning app is in list of untaggable apps' do
+      should 'return true if new record and owning app is migrated' do
         artefact = Artefact.new(owning_app: 'testapp')
         assert_equal true, artefact.tagging_migrated?
       end
 
-      should 'return false if new record and owning app is not in list of untaggable apps' do
+      should 'return false if new record and owning app not migrated' do
         artefact = Artefact.new(owning_app: 'whitehall')
-        assert_equal false, artefact.tagging_migrated?
-      end
-    end
-
-    context 'no untaggable apps specified' do
-      should 'return false if Settings.apps_with_migrated_tagging is nil' do
-        Settings.expects(:apps_with_migrated_tagging).returns(nil).at_least(1)
-        artefact = FactoryGirl.create(:artefact, slug: "low-hanging-fruit", owning_app: 'testapp')
         assert_equal false, artefact.tagging_migrated?
       end
     end
