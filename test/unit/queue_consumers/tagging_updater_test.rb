b0812ea2a0822de54e5a321ce7670008492e032d
Jack Scotti <jacopojackscotti@gmail.com>
2016-05-03 17:19:43 +0100

    Rename variables and parameters
    This makes tests easier to understand.
    Happy to remove this commit if this is valid only for myself.

--- test/unit/queue_consumers/tagging_updater_test.rb
+++ test/unit/queue_consumers/tagging_updater_test.rb
@@ -3,41 +3,41 @@ require 'govuk_message_queue_consumer/test_helpers'
 
 class TaggingUpdaterTest < ActiveSupport::TestCase
   def test_artefact_is_updated_with_tags
-    create(:live_tag, tag_id: 'my-tag', tag_type: 'section', content_id: 'MY-CONTENT-ID')
-    create(:live_tag, tag_id: 'parent-tag', tag_type: 'section', content_id: 'MY-PARENT')
-    create(:live_tag, tag_id: 'existing-tag', tag_type: 'section', content_id: 'A-CONTENT-ID')
+    create(:live_tag, tag_id: 'new-tag', tag_type: 'section', content_id: 'NEW-TAG-CONTENT-ID')
+    create(:live_tag, tag_id: 'parent-tag', tag_type: 'section', content_id: 'PARENT-TAG-CONTENT-ID')
+    create(:live_tag, tag_id: 'existing-tag', tag_type: 'section', content_id: 'EXISTING-TAG-CONTENT-ID')
     artefact = create(:artefact,
-      slug: 'a-tagged-item',
+      slug: 'an-item-with-links',
       sections: ["existing-tag"],
     )
     message = GovukMessageQueueConsumer::MockMessage.new({
-      "publishing_app" => "an-app-from-the-migrated-apps-config",
-      "base_path" => "/a-tagged-item",
+      "publishing_app" => "app-migrated-to-pub-api-v2-endpoints",
+      "base_path" => "/an-item-with-links",
       "links" => {
-        "mainstream_browse_pages" => ["MY-CONTENT-ID"],
-        "parent" => ["MY-PARENT"],
+        "mainstream_browse_pages" => ["NEW-TAG-CONTENT-ID"],
+        "parent" => ["PARENT-TAG-CONTENT-ID"],
       }
     })
 
     TaggingUpdater.new.process(message)
 
     artefact.reload
-    assert_equal ["parent-tag", "my-tag"], artefact.tags.map(&:tag_id)
+    assert_equal ["parent-tag", "new-tag"], artefact.tags.map(&:tag_id)
     assert message.acked?
   end
 
   def test_only_migrated_applications
-    create(:live_tag, tag_id: 'existing-tag', tag_type: 'section', content_id: 'A-CONTENT-ID')
+    create(:live_tag, tag_id: 'existing-tag', tag_type: 'section', content_id: 'EXISTING-TAG-CONTENT-ID')
     artefact = create(:artefact,
-                      slug: 'a-tagged-item',
+                      slug: 'an-item-with-links',
                       sections: ["existing-tag"],
                       owning_app: 'non-migrated-app',
                      )
 
     message = GovukMessageQueueConsumer::MockMessage.new({
       "publishing_app" => "non-migrated-app",
-      "base_path" => "/a-tagged-item",
-      "links" => { "mainstream_browse_pages" => ["MY-CONTENT-ID"] }
+      "base_path" => "/an-item-with-links",
+      "links" => { "mainstream_browse_pages" => ["NEW-TAG-CONTENT-ID"] }
     })
 
     TaggingUpdater.new.process(message)
@@ -49,9 +49,9 @@ class TaggingUpdaterTest < ActiveSupport::TestCase
 
   def test_when_no_artefact_found
     message = GovukMessageQueueConsumer::MockMessage.new({
-      "publishing_app" => "an-app-from-the-migrated-apps-config",
+      "publishing_app" => "app-migrated-to-pub-api-v2-endpoints",
       "base_path" => "/some-item-that-does-not-exist",
-      "links" => { "mainstream_browse_pages" => ["MY-CONTENT-ID"] }
+      "links" => { "mainstream_browse_pages" => ["NEW-TAG-CONTENT-ID"] }
     })
 
     TaggingUpdater.new.process(message)
@@ -62,7 +62,7 @@ class TaggingUpdaterTest < ActiveSupport::TestCase
   def test_when_links_are_missing
     artefact = create(:artefact, slug: 'an-item')
     message = GovukMessageQueueConsumer::MockMessage.new({
-      "publishing_app" => "an-app-from-the-migrated-apps-config",
+      "publishing_app" => "app-migrated-to-pub-api-v2-endpoints",
       "base_path" => "/an-item",
     })
 
