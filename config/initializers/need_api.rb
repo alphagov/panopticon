01ce3ead732e270971dbd602b65756532b4ee473
Tijmen Brommet <tijmen@gmail.com>
2016-05-06 11:42:36 +0100

    Remove need-related rake tasks
    This removes to rake tasks and files:
    
    - `import:mainstream_organisation_tags`. From what I can find, this was
    used to import the organisation-tagging from the need-api into
    panopticon.
    - `needs:import_cv`. This was a transitional tool to add needs from a
    static list.
    
    I’ve verified that these rake tasks are not run by the deployment
    scripts or cron job.

--- config/initializers/need_api.rb
+++
@@ -1,7 +0,0 @@
-require 'gds_api/base'
-require 'gds_api/need_api'
-
-Panopticon.need_api = GdsApi::NeedApi.new(Plek.current.find("need-api"),
-  timeout: 1,
-  bearer_token: ENV["panopticon_need_api_bearer_token"] || "change me when you deploy"
-)
