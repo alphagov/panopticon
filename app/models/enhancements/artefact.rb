1d00df371be325aae25601fcde91cce575179f0e
Tijmen Brommet <tijmen@gmail.com>
2016-05-06 12:10:38 +0100

    Move scopes into class instead of mixin
    The method to extract code into modules: provides a false sense that
    you have “thin models”, even though your public interface stays as big
    as it is.
    
    See
    http://blog.codeclimate.com/blog/2012/10/17/7-ways-to-decompose-fat-acti
    verecord-models/ for some thinking on this.
    
    It’s better to inline the definition here so that readers know what
    methods are exposed on the model.

--- app/models/enhancements/artefact.rb
+++ app/models/enhancements/artefact.rb
@@ -1,9 +1,6 @@
 require "artefact"
-require_relative "artefact/filter_scopes"
 
 class Artefact
-  include Artefact::FilterScopes
-
   NON_MIGRATED_APPS = %w(
     publisher
     whitehall
@@ -36,6 +33,27 @@ class Artefact
   scope :relatable_items_like, proc { |title_substring|
     relatable_items
       .any_of(:name => /\A#{Regexp.escape(title_substring)}/i)
+  }
+
+  scope :with_tags, proc {|tag_ids|
+    # the all_of method is used here so that, if this scope is called multiple
+    # times, the query will perform an intersection where artefacts match at least
+    # one tag in each list of IDs.
+    all_of(:tag_ids.in => tag_ids)
+  }
+  scope :with_parent_tag, proc {|tag_type, parent_tag_id|
+    tags = Tag.where(tag_type: tag_type, parent_id: parent_tag_id)
+    with_tags( [ parent_tag_id ] + tags.collect(&:tag_id) )
+  }
+
+  scope :of_kind, proc {|kind| where(kind: kind) }
+  scope :in_state, proc {|state| where(state: state) }
+  scope :owned_by, proc {|owning_app| where(owning_app: owning_app) }
+  scope :not_owned_by, proc {|owning_app| where(:owning_app.ne => owning_app) }
+
+  scope :matching_query, proc {|query|
+    search = /#{Regexp.escape(query)}/i
+    any_of({name: search}, {description: search}, {slug: search}, {kind: search}, {owning_app: search})
   }
 
   def related_artefact_slugs=(slugs)
