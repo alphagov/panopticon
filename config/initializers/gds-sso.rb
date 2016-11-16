56d4287ffe21ec94e8c465dd6807f17102526d09
Tijmen Brommet <tijmen@gmail.com>
2016-11-16 16:02:13 +0000

    Use correct oauth environment variables
    These are now set in hieradata.
    
    Trello: https://trello.com/c/LZBdyczv

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,7 +1,7 @@
 GDS::SSO.config do |config|
-  config.user_model   = "User"
-  config.oauth_id     = ENV['PANOPTICON_OAUTH_ID'] || "abcdefgh12345678pan"
-  config.oauth_secret = ENV['PANOPTICON_OAUTH_SECRET'] || "secret"
+  config.user_model = "User"
+  config.oauth_id = ENV['OAUTH_ID']
+  config.oauth_secret = ENV['OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
 end
 
